45b75d21dd1c29f467865c268dc8bdaae397d3dd
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-02-29 13:56:49 +0100

    Update calElmt.h

--- calElmt.h
+++ calElmt.h
@@ -2,14 +2,14 @@ void invertM2x2(float **M, float *det, float **M_inv);
 
 void matJacob(int nbneel, int dimDom, float **JacFk, float **S, float **Dfoncbase);
 
-void calDerFbase(int typel, float *x, float **dw);
+void calDerFbase(int nbneel, float *x, float **dw);
 
-void calFbase(int typel, float *x, float *w);
+void calFbase(int nbneel, float *x, float *w);
 
 void transFk(int nbneel, float **S, float Y[], float *foncbase);
 
-void numNaret(int typel, int aretNum, int ptsNum[]);
+void numNaret(int nbneel, int aretNum, int ptsNum[]);
 
 void selectPts(int nb, int num[], float *coorEns[], float *coorSel[]);
 
-void ppquad(int t, float *W, float **X);
+void ppquad(int nbneel, float *W, float **X);
