3ed4d9e75086e84220403cccb1c23e9d30d729ce
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-02-29 15:14:04 +0100

    Update cal1Elem.c

--- cal1Elem.c
+++ cal1Elem.c
@@ -1,48 +1,58 @@
 /* --------------------------------------
 Calcul elementaire sur l'element courant K :
 Arguments d'entrée :
-  tyepl    : type d'element
+  typel    : type d'element
   nbneel   : nombre de noeuds de l'element
   nbaret   : nombre d'aretes de l'element
-  coorEl   : tableau contenant des sommets de l'element courant K (à initialiser avec la fct selectPts à partir du fichier maillage)
+  coorEl   : tableau contenant les sommets de l'element courant K (à initialiser avec la fct selectPts à partir du fichier maillage)
   nRefArEl : tableau contenant les num de ref des nbaret de l'element
              float *nRefAr[nbaret]
   nRefDom  : etiquette coorespondant aux aretes internes
   nbRefD0  : taille de numRefD0
   numRefDO : numéro locaux des arretes coorespondant a une condition de Dirichlet  u=O
-  nbRefD1  : 
-  numRefD1 : numéro locaux des arretes coorespondant a une condition de Dirichlet non homogène   u=ud(x)
-  nbRefF1  : 
-  numRefF1 : numéro locaux des arretes coorespond a une condition de Neuman ou Fourier        eq diff
+  nbRefD1  : taille de numRefD1
+  numRefD1 : numéros locaux des arretes coorespondant a une condition de Dirichlet non homogène   u=ud(x)
+  nbRefF1  : taille de numRefF1
+  numRefF1 : numéros locaux des arretes coorespond a une condition de Neuman ou Fourier        eq diff
 
 Arguments de sortie :
-  MatElem  : 
-  SMbrElem : 
-  NuDElem  : 
-  uDElem   : 
+  MatElem  : éléments de la matrice élémentaire
+  SMbrElem : éléments du second membre
+  NuDElem  : noeuds qui ont la condition de dirichlet (0, 1, ou -1)
+  uDElem   : valeur de la condition de dirichlet
  ------------------------------------------- */ 
 #include "maille.h"
 #include "calElmt.h"
 #include "int.h"
 
-int cal1Elem( int typel, int nbneel, int nbaret, int nRefDom, float **coorEl, float *nRefArEl, 
+int cal1Elem(int nbneel, int nbaret, int nRefDom, float **coorEl, float *nRefArEl, 
   int nbRefD0, float *numRefD0, int nbRefD1, float *numRefD1, int nbRefF1, float *numRefF1, 
     float ** MatElem, float *SMbrElem, int *NuDElem, float *uDElem){
 
-  int i, j, k, R, numAr, cond_Ar;
-  int *num_noeuds;
-  float **coorAr;
-  float I_ar_w, I_ar_ww;
+  int i, j, k, l, nk, nl, R, numAr, cond_Ar;
+  int *num_noeuds, float *coorAr;
   
-  I_ar_w = malloc(2*sizeof(float));
-  if(I_ar_w == NULL){return 1;}
-  I_ar_ww = alloctab(2,2);
-  if(I_ar_ww == NULL){return 1;}
+  // Mini-matrice pour les aretes
+  float SMbrAret, MatAret;
+  SMbrAret = malloc(2*sizeof(float)); if(SMbrAret == NULL){return 1;}
+  MatAret = alloctab(2,2); if(MatAret == NULL){return 1;}
   
-  R = intElem(typel, coorEl, SMbrElem, MatElem)
+  //Initialisations de NuDElem, uDElem, SMbrElem et MatElem
+  for(i=0; i<nbneel; i++){
+    NuDElem[i]=0;
+    uDElem[i]=0;
+    SMbrElem[i]=0;
+    for(j=0; j<nbneel; j++){
+      MatElem[i][j]=0;
+    }
+  }
+  
+  //Calcul des intégrales surfaciques
+  R = intElem(nbneel, coorEl, SMbrElem, MatElem);
   if(R){return R};
-
-  for(i=0; i<nbaret ; i++){   // (i+1) numéro local de l'arrete
+  
+  //Prise en compte des conditions aux limites
+  for (i=0; i<nbaret ; i++){   // (i+1) numéro local de l'arrete
     cond_Ar=nRefArEl[i];
     if (cond_Ar == nRefDom){
       continue; // on passe au i suivant 
@@ -50,16 +60,16 @@ int cal1Elem( int typel, int nbneel, int nbaret, int nRefDom, float **coorEl, fl
     // Dirichlet homogène
     for (j=0; j<nbRefD0; j++){
       if (numRefD0[j]==cond_Ar){  
-        numNaret(typel, i+1, num_noeuds);  
-        NuDElem[num_noeuds[0]]=0;
-        NuDElem[num_noeuds[1]]=0;
+        numNaret(nbneel, i+1, num_noeuds);  
+        NuDElem[num_noeuds[0]]=1;
+        NuDElem[num_noeuds[1]]=1;        
         break;
       }
     }
     // Dirichlet non-homogène
     for(j=0; j<nbRefD1; j++){
       if (numRefD1[j]==cond_Ar) {  
-        numNaret(typel, i+1, num_noeuds);  
+        numNaret(nbneel, i+1, num_noeuds);  
         NuDElem[num_noeuds[0]]=-1;
         NuDElem[num_noeuds[1]]=-1;
         selectPts(2,num_noeuds, coorEl, coorAr);
@@ -71,21 +81,24 @@ int cal1Elem( int typel, int nbneel, int nbaret, int nRefDom, float **coorEl, fl
     // Neumann ou Fourier     // num-noeuds decale les indices
     for(j=0; j<nbRefF1; j++){
       if (numRefF1[j]==cond_Ar) {  
-        numNaret(typel, i+1, num_noeuds);  
+        numNaret(nbneel, i+1, num_noeuds);  
         selectPts(2,num_noeuds, coorEl, coorAr);
-        R = intAret(coorAr, I_ar_w, I_ar_ww) 
+        //Calcul des intégrales linéiques 
+        R = intAret(coorAr, SMbrAret, MatAret) 
         if(R){return R;}
-        for(j=0; j<nbneel; j++){
-          SMbrElem[num_noeuds[j]] += I_ar_w[j];
-          for(k=0; k<nbneel; k++){
-            MatElem[num_noeuds[j]][num_noeuds[k]-1]+=I_ar_ww[j][k]; 
+        for(k=0; k<nbneel; k++){
+          nk = num_noeuds[k]-1;
+          SMbrElem[nk] += SMbrAret[k];
+          for(l=0; l<nbneel; l++){
+            nl = num_noeuds[l]-1;
+            MatElem[nk][nl]+=MatAret[k][l]; 
           }
         }
        break;
       }
     }
   }
-  free(I_ar_w)
-  free(I_ar_ww)
+  free(SMbrAret)
+  free(MatAret)
   return 0;
 }
