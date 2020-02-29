cf818102b3505f7ed8af52d0f8f06c0e72b7ae28
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-02-29 18:10:49 +0100

    Update calElmt.h

--- calElmt.h
+++ calElmt.h
@@ -1,3 +1,5 @@
+void ppquad(int nbneel, float *W, float **X, int *nQuad);
+
 void invertM2x2(float **M, float *det, float **M_inv);
 
 void matJacob(int nbneel, int dimDom, float **JacFk, float **S, float **Dfoncbase);
@@ -11,5 +13,3 @@ void transFk(int nbneel, float **S, float Y[], float *foncbase);
 void numNaret(int nbneel, int aretNum, int ptsNum[]);
 
 void selectPts(int nb, int num[], float *coorEns[], float *coorSel[]);
-
-void ppquad(int nbneel, float *W, float **X);
