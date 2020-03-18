729c5473fb63b9734770f3d0acbab9906266565a
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-03-18 19:02:23 +0100

    Update syslin.h

--- syslin.h
+++ syslin.h
@@ -1,6 +1,6 @@
 int LecSMO(int *NbCoef, int *NbLign, float **MatriceO, int **AdPrCoefLiO,
             int **NumColO, float **SecMembreO);
 
-void dSMDaSMO(int *NbLign, float *SecMembreO, int *AdPrCoefLiO,
+int dSMDaSMO(int *NbLign, float *SecMembreO, int *AdPrCoefLiO,
               float *MatriceO, int *NumColO);
 
