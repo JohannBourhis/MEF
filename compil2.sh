5325ae7e651c029aba1cf4ebc5e96add9e73af6b
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-04-02 14:16:16 +0200

    Add files via upload

--- compil2.sh
+++ compil2.sh
@@ -1,2 +1,2 @@
 gfortran -c *.f
-gcc -Wall calElmt.c int.c maille.c impcalel.c alloctab.c syslin.c freetab.c assemblage.c testTP3.c *.o -lm -lgfortran
+gcc -Wall calElmt.c int.c fonc.c maille.c impcalel.c alloctab.c syslin.c freetab.c assemblage.c resol.c testTP3.c *.o -lm -lgfortran
---
+++ fonc.c
@@ -0,0 +1,146 @@
+extern int nucas;
+
+
+float A12(float *x){
+  return 0.0;
+}
+
+float A11(float *x){
+  return 1.0;
+}
+
+float A22(float *x){
+  return 1.0;
+}
+
+float A21(float *x){
+  return 0.0;
+}
+
+float A00(float *x){
+  if(nucas == 3){
+    return 1.0;
+  }else{
+    return 0.0;
+  }
+}
+
+float BN(float *x){
+  return 0.0;
+}
+
+float FOMEGA(float *x){
+  float val;
+  const float PI=3.141592;
+  switch(nucas){
+   case 1:
+     val = 32.*( x[1]*(1-x[1]) + x[0]*(1-x[0]) );
+     break;
+   case 2:
+     val = 2.*PI*PI*sin(PI*x[0])*sin(PI*x[1]);
+     break;
+   case 3:
+     val = (1+2.*PI*PI)*cos(PI*x[0])*cos(PI*x[1]);
+     break;
+   default:
+     printf("*** SOLEX : exemple non prevu. Abandon.\n");
+     exit(1);
+     break;
+  }
+  return val;
+}
+
+float FN(float *x, int condar){
+  const float PI=3.141592;
+  float val;
+
+  switch (nucas) {
+    case 1 :
+      switch (condar) {
+        case 1 :
+          val = -16.*(1-2*x[1])*(1-x[0])*x[0];
+          break;
+        case 2 :
+          val = 16.*(1-2*x[0])*(1-x[1])*x[1];
+          break;
+        case 3 :
+          val = 16.*(1-2*x[1])*(1-x[0])*x[0];
+          break;
+        case 4 :
+          val = -16.*(1-2*x[0])*(1-x[1])*x[1];
+          break;
+        default :
+          printf("*** FN : numéro de bord non prevu. Abandon.\n");
+          exit(1);
+          break;
+      }
+      break;
+    case 2 :
+      switch (condar) {
+        case 1 :
+          val = -PI*sin(PI*x[0])*cos(PI*x[1]);
+          break;
+        case 2 :
+          val = PI*sin(PI*x[1])*cos(PI*x[0]);
+          break;
+        case 3 :
+          val = PI*sin(PI*x[0])*cos(PI*x[1]);
+          break;
+        case 4 :
+          val = -PI*sin(PI*x[1])*cos(PI*x[0]);
+          break;
+        default :
+          printf("*** FN : numéro de bord non prevu. Abandon.\n");
+          exit(1);
+          break;
+      }
+      break;
+    case 3 :
+      switch (condar) {
+        case 1 :
+          val = PI*cos(PI*x[0])*sin(PI*x[1]);
+          break;
+        case 2 :
+          val = -PI*cos(PI*x[1])*sin(PI*x[0]);
+          break;
+        case 3 :
+          val = -PI*cos(PI*x[0])*sin(PI*x[1]);
+          break;
+        case 4 :
+          val = PI*cos(PI*x[1])*sin(PI*x[0]);
+          break;
+        default :
+          printf("*** FN : numéro de bord non prevu. Abandon.\n");
+          exit(1);
+          break;
+      }
+      break;
+    default :
+      printf("*** SOLEX : exemple non prevu. Abandon.\n");
+      exit(1);
+      break;
+  }
+  return(val);
+}
+
+float UD(float *x){ // correspond à la solution exacte des 3 exemples
+  const float PI=3.141592;
+  float val;
+
+  switch (nucas) {
+    case 1 :
+      val=16.*x[0]*x[1]*(1-x[0])*(1-x[1]);
+      break;
+    case 2 :
+      val=sin(PI*x[0])*sin(PI*x[1]);
+      break;
+    case 3 :
+      val=cos(PI*x[0])*cos(PI*x[1]);
+      break;
+    default :
+      printf("*** SOLEX : exemple non prevu. Abandon.\n");
+      exit(1);
+      break;
+  }
+  return(val);
+}
