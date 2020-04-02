e964c75b059ebe389f68f79ce59119eb321ce6f8
JohannBourhis <50465485+JohannBourhis@users.noreply.github.com>
2020-04-02 14:30:27 +0200

    Add files via upload

---
+++ Tests.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "maille.h"
+#include "calElmt.h"
+#include "int.h"
+#include "assemblage.h"
+#include "syslin.h"
+#include "forfun.h"
+#include "resol.h"
+
+int nucas;
+
+int main(){
+  char **FicMaillages =
+
+
+
+
+}
